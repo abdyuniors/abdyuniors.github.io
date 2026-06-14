d47a03b8bb636e79e2282a8aa7e363720804a453
abdyuniors <suekabdy14@gmail.com>
2026-06-14 14:56:14 +0800

    Update konfigurasi Astro untuk repository abdyuniors

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://abdyuniors.github.io",
+  base: "abdyuniors",
   vite: {
     plugins: [tailwindcss()],
   },
